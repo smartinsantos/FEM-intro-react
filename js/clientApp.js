ea901e04782e0b6e515476fabbcd5c9da1df6e6d
Sergio Martin <smartinsantos@gmail.com>
2017-04-03 09:36:14 -0500

    finished with props

--- js/clientApp.js
+++ js/clientApp.js
@@ -5,7 +5,7 @@ var MyTitle = React.createClass({
   render: function() {
     return (
       div(null,
-        h1(null, this.props.title))
+        h1({ style: { color: this.props.color }}, this.props.title))
     )
   }
 })
@@ -16,7 +16,7 @@ var MyFirstComponent = React.createClass({
   render: function () {
     return (
       div(null,
-        MyTitleFactory({title: 'props are the best'}),
+        MyTitleFactory({title: 'props are the best', color:'red' }),
         MyTitleFactory({title: 'another title sent by props'})
      )
     )
