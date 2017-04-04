a65347789e440755b6ed1f04fb4c96829a3aeccd
Sergio Martin <smartinsantos@gmail.com>
2017-04-04 07:15:32 -0500

    added routing

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,21 +1,22 @@
 // React
 import React from 'react'
 import { render } from 'react-dom'
-
+import { HashRouter, Match } from 'react-router'
 // CSS
 import '../public/normalize.css'
 import '../public/style.css'
 
+// Components
+import Landing from './Landing'
+
 var App = React.createClass({
   render () {
     return (
-      <div className='app'>
-        <div className='landing'>
-          <h1>rVideo</h1>
-          <input type='text' placeholder='Search' />
-          <a>or Browse All</a>
+      <HashRouter>
+        <div className='app'>
+          <Match exactly pattern='/' component={Landing} />
         </div>
-      </div>
+      </HashRouter>
     )
   }
 })
