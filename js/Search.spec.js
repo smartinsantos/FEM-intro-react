ce5ba75b343766287fe3623436dcfe1b14a2855f
Sergio Martin <smartinsantos@gmail.com>
2017-04-05 10:08:33 -0500

    added snapshots refactored babelrc

---
+++ js/Search.spec.js
@@ -0,0 +1,11 @@
+/* global test expect */
+
+import React from 'react'
+import Search from './Search'
+import renderer from 'react-test-renderer'
+
+test('Search snapshot test', () => {
+  const component = renderer.create(<Search />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
