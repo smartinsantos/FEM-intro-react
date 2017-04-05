491100c00a362d3b6e1bb29629234ef5470a30eb
Sergio Martin <smartinsantos@gmail.com>
2017-04-05 18:22:26 -0500

    created details component

--- js/Details.js
+++ js/Details.js
@@ -2,9 +2,25 @@ import React, { Component } from 'react'
 
 class Details extends Component {
   render () {
+    const { poster, title, year, description, trailer } = this.props.show
     return (
       <div className='details'>
-        <pre><code>{JSON.stringify(this.props.show, null, 4)}</code></pre>
+        <header>
+          <h1>rVideo</h1>
+        </header>
+        <section>
+          <h1>{title}</h1>
+          <h2>({year})</h2>
+          <img src={`/public/img/posters/${poster}`} />
+          <p>{description}</p>
+        </section>
+        <div>
+          <iframe
+            src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+            frameBorder='0'
+            allowFullScreen
+          />
+        </div>
       </div>
     )
   }
@@ -15,7 +31,8 @@ Details.propTypes = {
     poster: React.PropTypes.string,
     title: React.PropTypes.string,
     year: React.PropTypes.string,
-    description: React.PropTypes.string
+    description: React.PropTypes.string,
+    trailer: React.PropTypes.string
   })
 }
 
