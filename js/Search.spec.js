81a43fdfa09a35ff19c47e98190ed73835b254f3
Sergio Martin <smartinsantos@gmail.com>
2017-04-05 10:30:32 -0500

    added another test

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,16 +1,21 @@
 /* global test expect */
-
 import React from 'react'
-import Search from './Search'
-
 // enzyme
 import { shallow } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
+// Components
+import Search from './Search'
+import ShowCard from './ShowCard'
+import data from '../public/data.json'
 
 // jest library - make snapshot of the component rendered and when ever it changes test is going to fail
-
 test('Search snapshot test', () => {
   const component = shallow(<Search />)
   const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
+})
+
+test('Search should render a ShowCard for each show', () => {
+  const component = shallow(<Search />)
+  expect(component.find(ShowCard).length).toEqual(data.shows.length)
 })
