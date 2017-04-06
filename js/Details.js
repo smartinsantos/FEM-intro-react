ba160d697bbc06a48dc119a78f03aea92a6f78e8
Sergio Martin <smartinsantos@gmail.com>
2017-04-05 20:05:29 -0500

    using lifecycle to fetch data

--- js/Details.js
+++ js/Details.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 // Components
 import Header from './Header'
 
 class Details extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      omdbData: {}
+    }
+  }
+  componentDidMount () {
+    axios.get(`http://www.omdbapi.com/?i=${this.props.show.imdbID}`)
+    .then((res) => {
+      this.setState({ omdbData: res.data })
+    })
+    .catch((err) => {
+      console.error('axios error', err)
+    })
+  }
   render () {
     const { poster, title, year, description, trailer } = this.props.show
+
+    let rating = this.state.omdbData.imdbRating
+    ? <h3>{this.state.omdbData.imdbRating}</h3>
+    : <img src='/public/img/loading.png' alt='loading indicator' />
+
     return (
       <div className='details'>
         <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+          {rating}
           <img src={`/public/img/posters/${poster}`} />
           <p>{description}</p>
         </section>
@@ -33,7 +54,8 @@ Details.propTypes = {
     title: React.PropTypes.string,
     year: React.PropTypes.string,
     description: React.PropTypes.string,
-    trailer: React.PropTypes.string
+    trailer: React.PropTypes.string,
+    imdbID: React.PropTypes.string
   })
 }
 
--- js/Search.js
+++ js/Search.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import _ from 'lodash'
-
 // Components
 import ShowCard from './ShowCard'
 import Header from './Header'
