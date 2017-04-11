290f63725bc57422c64d1c4a7b97ffa8d241e092
Sergio <smartinsantos@gmail.com>
2017-04-11 16:37:49 -0500

    adding first action

--- js/App.js
+++ js/App.js
@@ -2,6 +2,9 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Match } from 'react-router'
+// REDUX
+import { Provider } from 'react-redux'
+import store from './redux/store'
 // CSS
 import '../public/normalize.css'
 import '../public/style.css'
@@ -17,23 +20,25 @@ import data from '../public/data.json'
 class App extends Component {
   render () {
     return (
-      <BrowserRouter>
-        <div className='app'>
-          <Match exactly pattern='/' component={Landing} />
-          <Match
-            pattern='/search'
-            component={(props) => <Search shows={data.shows} {...props} />}
-          />
-          <Match
-            pattern='/details/:id'
-            component={(props) => {
-              // filter the one show by its id, if not return null, then return details with the selected show
-              const show = data.shows.filter((show) => props.params.id === show.imdbID)[0] || null
-              return <Details show={show} {...props} />
-            }}
-          />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className='app'>
+            <Match exactly pattern='/' component={Landing} />
+            <Match
+              pattern='/search'
+              component={(props) => <Search shows={data.shows} {...props} />}
+            />
+            <Match
+              pattern='/details/:id'
+              component={(props) => {
+                // filter the one show by its id, if not return null, then return details with the selected show
+                const show = data.shows.filter((show) => props.params.id === show.imdbID)[0] || null
+                return <Details show={show} {...props} />
+              }}
+            />
+          </div>
+        </BrowserRouter>
+      </Provider>
     )
   }
 }
--- js/Landing.js
+++ js/Landing.js
@@ -1,16 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router'
 
-class Landing extends Component {
+// REDUX
+import { connect } from 'react-redux'
+import { setSearchTerm } from './redux/actionCreators'
+
+class Landing extends React.Component {
+  handleSearchTermChange (e) {
+    this.props.dispatch(setSearchTerm(e.target.value))
+  }
   render () {
+    const { searchTerm } = this.props
     return (
       <div className='landing'>
         <h1>rVideo</h1>
-        <input type='text' placeholder='Search' />
+        <input value={searchTerm} type='text' placeholder='Search' />
         <Link to='/search'>or Browse All</Link>
       </div>
     )
   }
 }
 
-export default Landing
+Landing.propTypes = {
+  searchTerm: React.PropTypes.string,
+  dispatch: React.PropTypes.func
+}
+
+// this function will passdown searchTerm as a prop to the Landing component
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+// this is how connect attaches th return value of mapStateToProps to Landing component
+export default connect(mapStateToProps)(Landing)
